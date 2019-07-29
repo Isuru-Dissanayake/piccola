9a8bab3240b47d2666e9e9d5c4095beabe5ee770
Isuru Dissanayake <isurumailme@gmail.com>
2019-07-29 17:26:23 +0530

    Create piccolaMain.ino

---
+++ code/piccolaMain.ino
@@ -0,0 +1,14 @@
+#include "piccolaPins.h"
+#include "piccolaVariables.h"
+#include "piccolaMotors.h"
+#include "piccolaEncoders.h"
+
+void setup()
+{
+    pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+    
+}
