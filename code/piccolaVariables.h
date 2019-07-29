12d03a16872acb7bffe788708500e12a26c112ac
Isuru Dissanayake <isurumailme@gmail.com>
2019-07-29 23:49:27 +0530

    Update piccolaVariables.h

--- code/piccolaVariables.h
+++ code/piccolaVariables.h
@@ -1,5 +1,5 @@
-int leftBase;
-int rightBase;
+int leftBase = 120;
+int rightBase = 120;
 int leftPwm;
 int rightPwm;
 
@@ -7,3 +7,25 @@ int leftEncoder;
 int rightEncoder;
 int encoderLeftCount;
 int encoderRightCount;
+
+float tof[5];
+
+float leftP = 7.0;
+float leftD = 3.5;
+
+float leftError;
+float leftLastError;
+
+float rightP = 7.0;
+float rightD = 3.5;
+
+float rightError;
+float rightLastError;
+
+float wallP = 7.0;
+float wallD = 3.5;
+
+float wallError;
+float wallLastError;
+
+float correction;
