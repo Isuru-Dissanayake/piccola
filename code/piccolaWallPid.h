9f11091fe472be4d2df5163f33d3b5cb9f781379
Isuru Dissanayake <isurumailme@gmail.com>
2019-07-30 21:49:57 +0530

    Update piccolaWallPid.h

--- code/piccolaWallPid.h
+++ code/piccolaWallPid.h
@@ -1,7 +1,8 @@
 void leftPid()
 {
     leftError = 35 - tof[0];
-    correction = (leftError * leftP) + (leftLastError * leftD);
+    correction = (leftError * leftP) + ((leftError - leftLastError) * leftD);
+    leftLastError = leftError;
     leftPwm = leftBase + correction;
     rightPwm = rightBase - correction;
 }
@@ -9,9 +10,18 @@ void leftPid()
 void rightPid()
 {
     rightError = 35 - tof[4];
+    correction  = (rightError * rightP) + ((rightError - rightLastError) * rightD);
+    rightLastError = rightError;
+    leftPwm = leftBase - correction;
+    rightPwm = rightBase + correction;
+
 }
 
 void wallPid()
 {
     wallError = tof[0] - tof[4];
+    correction = (wallError * wallP) + ((wallError - wallLastError) * wallD);
+    wallLastError = wallError;
+    leftPwm = leftBase - correction;
+    rightPwm = rightBase + correction;
 }
