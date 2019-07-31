5b27c996638a92d052a81cb9053ea75250272550
Isuru Dissanayake <isurumailme@gmail.com>
2019-07-31 15:44:36 +0530

    Update piccolaTof.h

--- code/piccolaTof.h
+++ code/piccolaTof.h
@@ -1,6 +1,86 @@
+VL6180X tof1;
+VL6180X tof2;
+VL6180X tof3;
+VL6180X tof4;
+VL6180X tof5;
+
+void tofSetup()
+{
+    pinMode(GPIO1, OUTPUT);
+    pinMode(GPIO2, OUTPUT);
+    pinMode(GPIO3, OUTPUT);
+    pinMode(GPIO4, OUTPUT);
+    pinMode(GPIO5, OUTPUT);
+    digitalWrite(GPIO1, LOW);
+    digitalWrite(GPIO2, LOW);
+    digitalWrite(GPIO3, LOW);
+    digitalWrite(GPIO4, LOW);
+    digitalWrite(GPIO5, LOW);
+
+    Wire.begin();
+
+    digitalWrite(GPIO1, HIGH);
+    delay(10);
+    tof1.init();
+    tof1.configureDefault();
+    //Sensor1.startRangeContinuous();
+    tof1.setTimeout(500);
+    tof1.setAddress(tofAddress1);
+
+    digitalWrite(GPIO2, HIGH);
+    delay(10);
+    tof2.init();
+    tof2.configureDefault();
+    //Sensor1.startRangeContinuous();
+    tof2.setTimeout(500);
+    tof2.setAddress(tofAddress2);
+
+    digitalWrite(GPIO3, HIGH);
+    delay(10);
+    tof3.init();
+    tof3.configureDefault();
+    //Sensor1.startRangeContinuous();
+    tof3.setTimeout(500);
+    tof3.setAddress(tofAddress3);
+
+    digitalWrite(GPIO4, HIGH);
+    delay(10);
+    tof4.init();
+    tof4.configureDefault();
+    //Sensor1.startRangeContinuous();
+    tof4.setTimeout(500);
+    tof4.setAddress(tofAddress4);
+
+    digitalWrite(GPIO5, HIGH);
+    delay(10);
+    tof5.init();
+    tof5.configureDefault();
+    //Sensor1.startRangeContinuous();
+    tof5.setTimeout(500);
+    tof5.setAddress(tofAddress5);
+}
+
 void tofRead()
 {
-    
+    tof[0] = tof1.readRangeSingleMillimeters();
+    tof[1] = tof2.readRangeSingleMillimeters();
+    tof[2] = tof3.readRangeSingleMillimeters();
+    tof[3] = tof4.readRangeSingleMillimeters();
+    tof[4] = tof5.readRangeSingleMillimeters();
+}
+
+void printTof()
+{
+    Serial.print(tof[0]);
+    Serial.print(",  ");
+    Serial.print(tof[1]);
+    Serial.print(",  ");
+    Serial.print(tof[2]);
+    Serial.print(",  ");
+    Serial.print(tof[3]);
+    Serial.print(",  ");
+    Serial.print(tof[4]);
+    Serial.println();
 }
 
 void checkWalls()
