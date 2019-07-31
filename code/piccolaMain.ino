6d287bbf8f42bbe9f6a0f70e2fc9770afcd8acf4
Isuru Dissanayake <isurumailme@gmail.com>
2019-07-31 15:44:25 +0530

    Update piccolaMain.ino

--- code/piccolaMain.ino
+++ code/piccolaMain.ino
@@ -1,17 +1,23 @@
+#include <Wire.h>
+#include <VL6180X.h>
+
 #include "piccolaPins.h"
 #include "piccolaVariables.h"
 #include "piccolaMotors.h"
 #include "piccolaTurns.h"
 #include "piccolaTof.h"
 #include "piccolaWallPid.h"
+#include "piccolaMazeSolve.h"
 
 void setup()
 {
     motorDiver();
     motorInterrupt();
+    tofSetup();
+    Serial.begin(9600);
 }
 
 void loop()
 {
-    
+
 }
