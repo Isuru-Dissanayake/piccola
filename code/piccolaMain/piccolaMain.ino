95d528b0fa4f345cd68d2e9c4a55241efb27e13b
Amaya Dharmasiri <32506203+theamaya@users.noreply.github.com>
2019-08-06 18:07:12 +0530

    Create piccolaMain.ino

---
+++ code/piccolaMain/piccolaMain.ino
@@ -0,0 +1,29 @@
+#include <Wire.h>
+#include <VL6180X.h>
+
+#include "piccolaPins.h"
+#include "piccolaVariables.h"
+#include "piccolaMotors.h"
+#include "piccolaTurns.h"
+#include "piccolaTof.h"
+#include "piccolaWallPid.h"
+#include "piccolaMazeSolve.h"
+
+void setup()
+{
+    motorDiver();
+    motorInterrupt();
+    tofSetup();
+
+    attachInterrupt(digitalPinToInterrupt(2), countLeftOut1, RISING);
+    attachInterrupt(digitalPinToInterrupt(3), countLeftOut2, RISING);
+    //attachInterrupt(digitalPinToInterrupt(4), countRightOut1, RISING);
+    //attachInterrupt(digitalPinToInterrupt(5), countRightOut1, RISING);
+
+    Serial.begin(9600);
+}
+
+void loop()
+{
+
+}
