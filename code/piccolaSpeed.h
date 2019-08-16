6af87d8f7d43a5ec0397a427980e06c09ec1fd67
isuru <isurumailme@gmail.com>
2019-08-15 18:19:21 -0700

    Update piccolaSpeed.h

--- code/piccolaSpeed.h
+++ code/piccolaSpeed.h
@@ -1,10 +1,37 @@
-void forwardStart()
-{   
-    
-}
-
-void forwardStop()
+void cellStart()
 {
+    rightEncoder=0;
+    leftEncoder=0;
+    encoderRightCount=640;
+    encoderLeftCount=640;
+    rightBase=60;
+    leftBase=60;
 
+    while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
+    {
+        rightBase = int(70+110/(1+pow(2.73,((300-rightEncoder)*0.012))));
+        leftBase = int(70+110/(1+pow(2.73,((300-leftEncoder)*0.012))));
+        forwardBase();
+    }
+    rightBase=177;
+    leftBase=180;
+    //forwardBase();
 }
 
+void cellBrake()
+{
+    rightEncoder=0;
+    leftEncoder=0;
+    encoderRightCount=620;
+    encoderLeftCount=620;
+    rightBase=177;
+    leftBase=180;
+    while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
+
+    {
+        rightBase = int(180-110/(1+pow(2.73,((300-rightEncoder)*0.012))));
+        leftBase = int(180-110/(1+pow(2.73,((300-leftEncoder)*0.012))));
+        forwardBase();  
+    }
+    brake();
+}
