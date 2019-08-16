9989a7bf7dc8d36d48dae3c90169fa6ec417bbf4
isuru <isurumailme@gmail.com>
2019-08-15 18:19:16 -0700

    Update piccolaMazeSolve.h

--- code/piccolaMazeSolve.h
+++ code/piccolaMazeSolve.h
@@ -3,7 +3,14 @@ void mazeSolve()
     switch (nextMove)
     {
     case 'F' :
-        //move forward
+        leftEncoder = 0;
+        rightEncoder = 0;
+        encoderLeftCount = 1280;
+        encoderRightCount = 1280;
+        while (leftEncoder <= encoderLeftCount || rightEncoder <= encoderRightCount)
+        {
+            forwardBase();
+        }
         break;
     
     case 'R' :
