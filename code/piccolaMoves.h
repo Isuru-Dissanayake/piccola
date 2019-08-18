2065f61b8be4a485bb211e976bac8a1a707c20d3
isuru <isurumailme@gmail.com>
2019-08-18 14:04:24 -0700

    Update piccolaMoves.h

--- code/piccolaMoves.h
+++ code/piccolaMoves.h
@@ -12,6 +12,42 @@ void cellForward()
     }
 }
 
+void rightTurn()
+{
+    leftEncoder = 0;
+    rightEncoder = 0;
+    encoderLeftCount = 1550;
+    leftPwm = leftBase + 75;
+    rightPwm = rightBase - 81;
+    while (leftEncoder <= encoderLeftCount)
+    {
+        forward();
+    }
+    //brake();
+    leftEncoder = 0;
+    rightEncoder = 0;
+    encoderLeftCount = 0;
+    encoderRightCount = 0;
+}
+
+void leftTurn()
+{
+    leftEncoder = 0;
+    rightEncoder = 0;
+    encoderRightCount = 1555;
+    leftPwm = leftBase - 75;
+    rightPwm = rightBase + 75;
+    while (rightEncoder <= encoderRightCount)
+    {
+        forward();
+    }
+    //brake();
+    leftEncoder = 0;
+    rightEncoder = 0;
+    encoderLeftCount = 0;
+    encoderRightCount = 0;
+}
+
 void cellBack()
 {
     cellBrake();
