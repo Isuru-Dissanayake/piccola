bc77bc261254d7db4aec83d94898a3d6a639e656
isuru <isurumailme@gmail.com>
2019-08-18 14:39:50 -0700

    Update piccolaMazeSolve.h

--- code/piccolaMazeSolve.h
+++ code/piccolaMazeSolve.h
@@ -1,11 +1,32 @@
 void mazeSolve()
 {
     tofCell();
+    checkWallsCell();
+    if (cellWalls[2] == 0)
+    {
+        nextMove = 'R';
+    }
+
+    else if (cellWalls[1] == 0)
+    {
+        nextMove = 'F';
+    }
+
+    else if (cellWalls[0] == 0)
+    {
+        nextMove = 'L';
+    }
+
+    else if (cellWalls[0] == 1)
+    {
+        nextMove = 'B';
+    }
+
     
     switch (nextMove)
     {
     case 'F' :      //move forward
-        cellForwad();
+        cellForward();
         break;
     case 'R' :      //move right
         rightTurn();
