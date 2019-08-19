197de462f69cecf40790891cbe99cc5f93ef928d
isuru <isurumailme@gmail.com>
2019-08-18 19:01:10 -0700

    Update piccolaMoves.h

--- code/piccolaMoves.h
+++ code/piccolaMoves.h
@@ -16,16 +16,33 @@ void rightTurn()
 {
     leftEncoder = 0;
     rightEncoder = 0;
-    encoderLeftCount = 1550;
-    leftPwm = leftBase + 75;
-    rightPwm = rightBase - 81;
+    encoderLeftCount = 1400;
+    while (leftEncoder <= 50)
+    {
+        leftBase = int(180+75/(1+pow(2.73,((25-rightEncoder)*0.05))));
+        rightBase = int(176-115/(1+pow(2.73,((25-leftEncoder)*0.05))));
+        forwardBase();
+    }
+    leftEncoder = 0;
+    rightEncoder = 0;
     while (leftEncoder <= encoderLeftCount)
     {
-        forward();
+        forwardBase();
+    }
+
+    leftEncoder = 0;
+    rightEncoder = 0;
+    while (leftEncoder <= 60)
+    {
+        leftBase = int(255-75/(1+pow(2.73,((25-rightEncoder)*0.05))));
+        rightBase = int(56+130/(1+pow(2.73,((25-leftEncoder)*0.05))));
+        forwardBase();
     }
     //brake();
     leftEncoder = 0;
     rightEncoder = 0;
+    leftBase = 180;
+    rightBase = 176;
     encoderLeftCount = 0;
     encoderRightCount = 0;
 }
@@ -34,16 +51,44 @@ void leftTurn()
 {
     leftEncoder = 0;
     rightEncoder = 0;
-    encoderRightCount = 1555;
-    leftPwm = leftBase - 75;
-    rightPwm = rightBase + 75;
+    encoderRightCount = 1410;
+    while (rightEncoder <= 50)
+    {
+        rightBase = int(176+79/(1+pow(2.73,((25-rightEncoder)*0.05))));
+        leftBase = int(180-90/(1+pow(2.73,((25-leftEncoder)*0.05))));
+        forwardBase();
+    }
+    leftBase = 90;
+    rightBase = 255;
+    leftEncoder = 0;
+    rightEncoder = 0;
     while (rightEncoder <= encoderRightCount)
     {
-        forward();
+        forwardBase();
+    }
+
+    leftEncoder = 0;
+    rightEncoder = 0;
+    while (rightEncoder <= 50)
+    {
+        rightBase = int(255-79/(1+pow(2.73,((25-rightEncoder)*0.05))));
+        leftBase = int(90+90/(1+pow(2.73,((25-leftEncoder)*0.05))));
+        forwardBase();
+    }
+
+    leftEncoder = 0;
+    rightEncoder = 0;
+    leftBase = 180;
+    rightBase = 176;
+    while (rightEncoder <= 10)
+    {
+        forwardBase();
     }
     //brake();
     leftEncoder = 0;
     rightEncoder = 0;
+    leftBase = 180;
+    rightBase = 176;
     encoderLeftCount = 0;
     encoderRightCount = 0;
 }
