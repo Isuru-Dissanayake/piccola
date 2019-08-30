4d76e6a99b853f31df2c3fbc0dd46464da2a5c8a
isuru <isurumailme@gmail.com>
2019-08-30 12:49:23 -0700

    Create piccolaMoves.h

---
+++ piccolaMain/piccolaMoves.h
@@ -0,0 +1,132 @@
+void mazeStart()
+{
+    rightEncoder=0;
+    leftEncoder=0;
+    encoderRightCount=900;
+    encoderLeftCount=900;
+    rightBase=60;
+    leftBase=60;
+
+    while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
+    {
+        rightBase = int(70+110/(1+pow(2.73,((300-rightEncoder)*0.012))));
+        leftBase = int(70+110/(1+pow(2.73,((300-leftEncoder)*0.012))));
+        forwardBase();
+    }
+    rightBase=177;
+    leftBase=180;
+    //forwardBase();
+}
+
+void cellForward()
+{
+    leftEncoder = 0;
+    rightEncoder = 0;
+    encoderLeftCount = 1275;
+    encoderRightCount = 1275;
+    while (leftEncoder <= encoderLeftCount || rightEncoder <= encoderRightCount)
+    {
+        tofPid();
+        wallFollow();
+    }
+}
+
+
+void rightTurn()
+{
+    leftEncoder = 0;
+    encoderLeftCount = 1250;
+    while (leftEncoder <= 50)
+    {
+        leftBase = int(180+75/(1+pow(2.73,((25-leftEncoder)*0.05))));
+        rightBase = int(176-90/(1+pow(2.73,((25-rightEncoder)*0.05))));
+        forwardBase();
+    }
+    leftBase = 255;
+    rightBase = 86;
+    leftEncoder = 0;
+    while (leftEncoder <= encoderLeftCount)
+    {
+        forwardBase();
+    }
+    leftEncoder = 0;
+    while (leftEncoder <= 60)
+    {
+        leftBase = int(255-75/(1+pow(2.73,((30-rightEncoder)*0.05))));
+        rightBase = int(86+90/(1+pow(2.73,((30-leftEncoder)*0.05))));
+        forwardBase();
+    }
+    leftEncoder = 0;
+    leftBase = 180;
+    rightBase = 176;
+    while (leftEncoder <= 50)
+    {
+        forwardBase();
+    }
+    leftEncoder = 0;
+    rightEncoder = 0;
+    encoderLeftCount = 0;
+    encoderRightCount = 0;
+}
+
+
+void leftTurn()
+{
+    leftEncoder = 0;
+    rightEncoder = 0;
+    encoderRightCount = 1340;
+    while (rightEncoder <= 50)
+    {
+        rightBase = int(176+70/(1+pow(2.73,((25-rightEncoder)*0.05))));
+        leftBase = int(180-95/(1+pow(2.73,((25-leftEncoder)*0.05))));
+        forwardBase();
+    }
+    leftBase = 85;
+    rightBase = 246;
+    leftEncoder = 0;
+    rightEncoder = 0;
+    while (rightEncoder <= encoderRightCount)
+    {
+        forwardBase();
+    }
+
+    leftEncoder = 0;
+    rightEncoder = 0;
+    while (rightEncoder <= 50)
+    {
+        rightBase = int(246-70/(1+pow(2.73,((25-rightEncoder)*0.05))));
+        leftBase = int(85+95/(1+pow(2.73,((25-leftEncoder)*0.05))));
+        forwardBase();
+    }
+
+    leftEncoder = 0;
+    rightEncoder = 0;
+    leftBase = 180;
+    rightBase = 176;
+    while (rightEncoder <= 60)
+    {
+        tof[2] = tof3.readRangeSingleMillimeters();
+        if (tof[2] <= 138)
+        {
+            //brake();
+            break;
+        }
+        else
+        {
+            forwardBase();
+        }
+    }
+    leftEncoder = 0;
+    rightEncoder = 0;
+    leftBase = 180;
+    rightBase = 176;
+    encoderLeftCount = 0;
+    encoderRightCount = 0;
+}
+
+void cellBack()
+{
+    cellBrake();
+    turnBack();
+    cellStart();
+}
