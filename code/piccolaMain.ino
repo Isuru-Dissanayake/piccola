f461228be2cfc028b8804388055107a5730f7894
isuru <isurumailme@gmail.com>
2019-08-29 20:24:18 -0700

    Update piccolaMain.ino

--- code/piccolaMain.ino
+++ code/piccolaMain.ino
@@ -18,32 +18,10 @@ void setup()
     motorDiver();
     motorInterrupt();
     attachInterrupt(digitalPinToInterrupt(PB12), countLeftOut1, RISING);
-    attachInterrupt(digitalPinToInterrupt(PB13), countLeftOut2, RISING);
+    //attachInterrupt(digitalPinToInterrupt(PB13), countLeftOut2, RISING);
     attachInterrupt(digitalPinToInterrupt(PB14), countRightOut1, RISING);
-    attachInterrupt(digitalPinToInterrupt(PB15), countRightOut2, RISING);
+    //attachInterrupt(digitalPinToInterrupt(PB15), countRightOut2, RISING);
     Serial2.begin(9600);
-}
-
-void loopww()
-{
-    delay(1000);
-    mazeStart();
-    brake();
-    delay(1000);
-    while(1)
-    {
-        mazeSolve();
-    }
-    
-}
-
-void loopj()
-{
-    tofPid();
-    //printTof();
-    wallPid();
-    forward();
-    
 }
 
 void loop()
@@ -52,7 +30,6 @@ void loop()
     cellStart();
     cellBrake();
     cellStart();
-    cellForward();
     cellForward();
     cellBrake();
     
