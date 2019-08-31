772fa2705dada8e4653506cdac7844f49a484403
KavindaDulhan <kavindadulhan@gmail.com>
2019-08-31 21:35:00 +0530

    Update piccolaSpeed.h

--- code/piccolaSpeed.h
+++ code/piccolaSpeed.h
@@ -1,21 +1,21 @@
+
 void cellStart()
 {
-    rightEncoder=0;
-    leftEncoder=0;
-    encoderRightCount=330;
-    encoderLeftCount=330;
+    encoderRightCount = rightEncoder + 300;
+    encoderLeftCount = leftEncoder + 300;
     rightBase=70;
     leftBase=70;
-    while (rightEncoder <= 300 || leftEncoder <= 300)
+    while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
     {
-        rightBase = int(70+110/(1+pow(2.73,((100-leftEncoder)*0.05))));
-        leftBase = int(70+110/(1+pow(2.73,((100-leftEncoder)*0.05))));
+        dif = leftEncoder - encoderLeftCount + 300;
+        rightBase = int(70+110/(1+pow(2.73,((150-dif)*0.05))));
+        leftBase = int(70+110/(1+pow(2.73,((150-dif)*0.05))));
         forwardBase();
     }
     rightBase=176;
     leftBase=180;
-    rightEncoder=0;
-    leftEncoder=0;
+    encoderRightCount= rightEncoder + 300;
+    encoderLeftCount= leftEncoder + 100;
     while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
     {
         tofPid();
@@ -26,23 +26,25 @@ void cellStart()
 
 void cellBrake()
 {
-    rightEncoder=0;
-    leftEncoder=0;
-    encoderRightCount=330;
-    encoderLeftCount=330;
+    encoderRightCount= rightEncoder + 300;
+    encoderLeftCount= leftEncoder + 300;
     rightBase=176;
     leftBase=180;
     while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
     {
-        forwardBase();
+        tofPid();
+        wallFollow();
+        //forwardBase();
     }
-    rightEncoder=0;
-    leftEncoder=0;
-    while (rightEncoder <= 300 || leftEncoder <= 300)
+    encoderRightCount= rightEncoder + 300;
+    encoderLeftCount= leftEncoder + 300;
+    while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
     {   
-        rightBase = int(176-110/(1+pow(2.73,((150-leftEncoder)*0.05))));
-        leftBase = int(180-110/(1+pow(2.73,((150-leftEncoder)*0.05))));
+        dif = leftEncoder - encoderLeftCount + 300;
+        rightBase = int(180-110/(1+pow(2.73,((150-dif)*0.05))));
+        leftBase = int(180-110/(1+pow(2.73,((150-dif)*0.09))));
         forwardBase();
     }
     brake();
 }
+
