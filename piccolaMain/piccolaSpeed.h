e23da8d9a31ea251146fb1076acf8b30ce190a06
Amaya Dharmasiri <32506203+theamaya@users.noreply.github.com>
2019-08-31 11:23:59 +0530

    Update piccolaSpeed.h

--- piccolaMain/piccolaSpeed.h
+++ piccolaMain/piccolaSpeed.h
@@ -2,17 +2,21 @@ void cellStart()
 {
     rightEncoder=0;
     leftEncoder=0;
-    encoderRightCount=635;
-    encoderLeftCount=635;
-    rightBase=70;
-    leftBase=70;
-
+    encoderRightCount=620;
+    encoderLeftCount=620;
+    //rightBase=70;
+    //leftBase=70;
+    rightBase=176;
+    leftBase=180;
     while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
     {
-        rightBase = int(70+110/(1+pow(2.73,((319-rightEncoder)*0.012))));
-        leftBase = int(70+110/(1+pow(2.73,((319-leftEncoder)*0.012))));
-        forwardBase();
+        //rightBase = int(70+110/(1+pow(2.73,((310-rightEncoder)*0.012))));
+        //leftBase = int(70+110/(1+pow(2.73,((310-leftEncoder)*0.012))));
+        //forwardBase();
+        tofPid();
+        wallFollow();
     }
+    
     rightBase=176;
     leftBase=180;
     //forwardBase();
@@ -22,16 +26,20 @@ void cellBrake()
 {
     rightEncoder=0;
     leftEncoder=0;
-    encoderRightCount=635;
-    encoderLeftCount=635;
+    encoderRightCount=620;
+    encoderLeftCount=620;
     rightBase=176;
     leftBase=180;
+    
     while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
-
-    {
-        rightBase = int(176-110/(1+pow(2.73,((319-rightEncoder)*0.012))));
-        leftBase = int(180-110/(1+pow(2.73,((319-leftEncoder)*0.012))));
-        forwardBase();  
+    {   
+        //rightBase = int(176-110/(1+pow(2.73,((310-rightEncoder)*0.012))));
+        //leftBase = int(180-110/(1+pow(2.73,((310-leftEncoder)*0.012))));
+        //forwardBase();
+        tofPid();
+        wallFollow();
     }
-    brake();
+    //brake();
+    leftBase=180;
+    rightBase=176;
 }
