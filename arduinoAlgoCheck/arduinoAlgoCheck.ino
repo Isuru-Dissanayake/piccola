4eff5fc48d42a9d6dad6da7974557c1063e7a498
Amaya Dharmasiri <32506203+theamaya@users.noreply.github.com>
2019-08-31 21:33:06 +0530

    Update arduinoAlgoCheck.ino

--- arduinoAlgoCheck/arduinoAlgoCheck.ino
+++ arduinoAlgoCheck/arduinoAlgoCheck.ino
@@ -1,28 +1,7 @@
-#include "piccolaZlgo.h"
+#include "piccolaZlgoVariables.h"
 #include "piccolaZlgoSupport.h"
+#include "piccolaZlgo.h"
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  x_0=0;
-}
-
-void loop() {
-  Serial.println(' ');
-  Serial.println(' ');
-  //showFlood();
-  Serial.println(' ');
-  Serial.println(' ');
-  //showCells();
-  
-  getWalls();
-  
-  
-}
-
-void loopu(){
-  Serial.println(x_0);
-  y_0=x_0-1;
-  Serial.println(y_0);
-  
+  Serial2.begin(9600);
 }
