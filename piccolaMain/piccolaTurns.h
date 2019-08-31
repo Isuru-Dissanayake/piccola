0470f5ba559776f09d45fdc36baaff8d7646b42a
KavindaDulhan <kavindadulhan@gmail.com>
2019-08-31 21:35:45 +0530

    Update piccolaTurns.h

--- piccolaMain/piccolaTurns.h
+++ piccolaMain/piccolaTurns.h
@@ -8,124 +8,102 @@ void countRightOut1()
     rightEncoder = rightEncoder + 1;
 }
 
-
-void turnRightEncoder()
-{   
-    leftEncoder = 0;
-    rightEncoder = 0;
-    while (leftEncoder <= encoderLeftCount || rightEncoder <= encoderRightCount)
-    {
-        if (rightEncoder >= encoderRightCount) 
-        {
-            rightReverseBase();
-        }
-        else
-        {
-            rightBrake();
-        }
-        
-        if (leftEncoder <= encoderLeftCount)
-        {
-            leftForwardBase();
-        }
-        else
-        {
-            leftBrake();
-        } 
-    }
-    brake();
-    encoderLeftCount = 0;
-    encoderRightCount = 0;
-    leftEncoder = 0;
-    rightEncoder = 0;
-}
-
-void turnLeftEncoder()
-{   
-    leftEncoder = 0;
-    rightEncoder = 0;
-    while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
-    {
-        if (rightEncoder <= encoderRightCount) 
-        {
-            rightForwardBase();
-        }
-        else
-        {
-
-
-            rightBrake();
-        }
-        
-        if (leftEncoder >= encoderLeftCount)
-        {
-            leftReverseBase();
-        }
-        else
-        {
-            leftBrake();
-        }
-        
-    }
-    brake();
-    encoderLeftCount = 0;
-    encoderRightCount = 0;
-    leftEncoder = 0;
-    rightEncoder = 0;
-}
-
 void turnBack()
 {
-    rightBase=177;
-    leftBase=180;
-    encoderLeftCount = 930;
-    encoderRightCount = 930;
-    turnRightEncoder();
-}
-
-void rightAboutTurn(){
-  leftEncoder=0;
-  rightEncoder=0;
-  while (rightEncoder <= 100 || leftEncoder <= 100)
+    encoderRightCount= rightEncoder + 100;
+    encoderLeftCount= leftEncoder + 100;
+    while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
     {
-        rightBase = int(70+110/(1+pow(2.73,((100-rightEncoder)*0.025))));
-        leftBase = int(70+110/(1+pow(2.73,((100-leftEncoder)*0.025))));
+        dif = leftEncoder - encoderLeftCount + 100;
+        rightBase = int(70+110/(1+pow(2.73,((50-dif)*0.05))));
+        leftBase = int(70+110/(1+pow(2.73,((50-dif)*0.05))));
         turnRight();
     }
     rightBase=176;
     leftBase=180;
-    
-  while(rightEncoder <= 350){
-  turnRight();}
-   while (rightEncoder <= 450 || leftEncoder <= 450)
-
+    encoderRightCount= rightEncoder + 703;
+    encoderLeftCount= leftEncoder + 703;
+    while(rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
     {
-        rightBase = int(176-110/(1+pow(2.73,((100-rightEncoder)*0.025))));
-        leftBase = int(180-110/(1+pow(2.73,((100-leftEncoder)*0.025))));
+        turnRight();
+    }
+    encoderRightCount= rightEncoder + 100;
+    encoderLeftCount= leftEncoder + 100;
+    while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
+    {
+        dif = leftEncoder - encoderLeftCount + 100;
+        rightBase = int(176-110/(1+pow(2.73,((50-dif)*0.05))));
+        leftBase = int(180-110/(1+pow(2.73,((50-dif)*0.05))));
         turnRight();  
     }
     brake();
+    leftBase = 180;
+    rightBase = 176;
 }
-void leftAboutTurn(){
-  leftEncoder=0;
-  rightEncoder=0;
-  while (rightEncoder <= 100 || leftEncoder <= 100)
+
+void rightAboutTurn()
+{
+    encoderRightCount= rightEncoder + 100;
+    encoderLeftCount= leftEncoder + 100;
+    while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
     {
-        rightBase = int(70+110/(1+pow(2.73,((100-rightEncoder)*0.025))));
-        leftBase = int(70+110/(1+pow(2.73,((100-leftEncoder)*0.025))));
+        dif = leftEncoder - encoderLeftCount + 100;
+        rightBase = int(70+110/(1+pow(2.73,((50-dif)*0.05))));
+        leftBase = int(70+110/(1+pow(2.73,((50-dif)*0.05))));
+        turnRight();
+    }
+    rightBase=176;
+    leftBase=180;
+    encoderRightCount= rightEncoder + 240;
+    encoderLeftCount= leftEncoder + 240;
+    while(rightEncoder <= encoderRightCount)
+    {
+        turnRight();
+    }
+    encoderRightCount= rightEncoder + 100;
+    encoderLeftCount= leftEncoder + 100;
+    while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
+    {
+        dif = leftEncoder - encoderLeftCount + 100;
+        rightBase = int(176-110/(1+pow(2.73,((50-dif)*0.05))));
+        leftBase = int(180-110/(1+pow(2.73,((50-dif)*0.05))));
+        turnRight();  
+    }
+    brake();
+    leftBase = 180;
+    rightBase = 176;
+}
+
+void leftAboutTurn()
+{
+    encoderRightCount= rightEncoder + 100;
+    encoderLeftCount= leftEncoder + 100;
+    while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
+    {
+        dif = leftEncoder - encoderLeftCount + 100;
+        rightBase = int(70+110/(1+pow(2.73,((50-dif)*0.05))));
+        leftBase = int(70+110/(1+pow(2.73,((50-dif)*0.05))));
         turnLeft();
     }
     rightBase=176;
     leftBase=180;
-    
-  while(rightEncoder <= 362){
-  turnLeft();}
-   while (rightEncoder <= 462 || leftEncoder <= 462)
-
+    encoderRightCount= rightEncoder + 262;
+    encoderLeftCount= leftEncoder + 262;
+    while(rightEncoder <= encoderRightCount)
     {
-        rightBase = int(176-110/(1+pow(2.73,((100-rightEncoder)*0.025))));
-        leftBase = int(180-110/(1+pow(2.73,((100-leftEncoder)*0.025))));
+        turnLeft();
+    }
+    
+    encoderRightCount= rightEncoder + 100;
+    encoderLeftCount= leftEncoder + 100;
+    while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
+    {
+        dif = leftEncoder - encoderLeftCount + 100;
+        rightBase = int(176-110/(1+pow(2.73,((50-dif)*0.05))));
+        leftBase = int(180-110/(1+pow(2.73,((50-dif)*0.05))));
         turnLeft();  
     }
     brake();
+    leftBase = 180;
+    rightBase = 176;
 }
