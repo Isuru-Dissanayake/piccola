6c28b4c31a23aacbbf72a7c1a5a305780981b0c8
KavindaDulhan <kavindadulhan@gmail.com>
2019-08-31 21:34:53 +0530

    Update piccolaMazeSolve.h

--- code/piccolaMazeSolve.h
+++ code/piccolaMazeSolve.h
@@ -1,14 +1,16 @@
 void mazeSolve()
 {
-    //brake();
-    //delay(1000);
     //time = millis();
-    //brake();
-    tofCell();
-    tofCell();
-    checkWallsCell();
+    //tofCell();
+    //tofCell();
+    //checkWallsCell();
     //printWallState();
     //delay(1000);
+    //brake();
+    //delay(200);
+    tofPid();
+    tofPid();
+    checkWallsCell();
     if (cellWalls[2] == 0)
     {
         nextMove = 'R';
@@ -28,10 +30,12 @@ void mazeSolve()
     {
         nextMove = 'B';
     }
-    //Serial2.print(millis() - time);
-    //Serial2.print("   ");
-    //Serial2.println(nextMove);
-    //delay(1000);
+    /*
+    Serial2.print(millis() - time);
+    Serial2.print("   ");
+    Serial2.println(nextMove);
+    delay(1000);
+    */
     switch (nextMove)
     {
     case 'F' :      //move forward
