389f47982a60b9548d9ff9981549bc84249653fb
isuru <isurumailme@gmail.com>
2019-08-31 12:30:04 -0700

    Update piccolaTurns.h

--- code/piccolaTurns.h
+++ code/piccolaTurns.h
@@ -13,7 +13,7 @@ void turnRightEncoder()
 {   
     leftEncoder = 0;
     rightEncoder = 0;
-    while (leftEncoder <= encoderLeftCount || rightEncoder >= encoderRightCount)
+    while (leftEncoder <= encoderLeftCount || rightEncoder <= encoderRightCount)
     {
         if (rightEncoder >= encoderRightCount) 
         {
@@ -44,7 +44,7 @@ void turnLeftEncoder()
 {   
     leftEncoder = 0;
     rightEncoder = 0;
-    while (rightEncoder <= encoderRightCount || leftEncoder >= encoderLeftCount)
+    while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
     {
         if (rightEncoder <= encoderRightCount) 
         {
@@ -74,25 +74,62 @@ void turnLeftEncoder()
     rightEncoder = 0;
 }
 
-void turnNinetyLeft()
-{
-    encoderRightCount = 440;
-    encoderLeftCount = -440;
-    turnLeftEncoder();
-}
-
-void turnNinetyRight()
-{
-    encoderLeftCount = 440;
-    encoderRightCount = -440;
-    turnRightEncoder();
-}
-
 void turnBack()
 {
     rightBase=177;
     leftBase=180;
     encoderLeftCount = 930;
-    encoderRightCount = -930;
+    encoderRightCount = 930;
     turnRightEncoder();
+}
+
+void rightAboutTurn()
+{
+    leftEncoder=0;
+    rightEncoder=0;
+    while (rightEncoder <= 100 || leftEncoder <= 100)
+    {
+        rightBase = int(70+110/(1+pow(2.73,((50-rightEncoder)*0.05))));
+        leftBase = int(70+110/(1+pow(2.73,((50-leftEncoder)*0.05))));
+        turnRight();
+    }
+    rightBase=176;
+    leftBase=180;
+    while(rightEncoder <= 350)
+    {
+        turnRight();
+    }
+    while (rightEncoder <= 450 || leftEncoder <= 450)
+    {
+        rightBase = int(176-110/(1+pow(2.73,((50-rightEncoder)*0.05))));
+        leftBase = int(180-110/(1+pow(2.73,((50-leftEncoder)*0.05))));
+        turnRight();  
+    }
+    brake();
+}
+
+void leftAboutTurn()
+{
+    leftEncoder=0;
+    rightEncoder=0;
+    while (rightEncoder <= 100 || leftEncoder <= 100)
+    {
+        rightBase = int(70+110/(1+pow(2.73,((50-rightEncoder)*0.05))));
+        leftBase = int(70+110/(1+pow(2.73,((50-leftEncoder)*0.05))));
+        turnLeft();
+    }
+    rightBase=176;
+    leftBase=180;
+    
+    while(rightEncoder <= 362)
+    {
+        turnLeft();
+    }
+    while (rightEncoder <= 462 || leftEncoder <= 462)
+    {
+        rightBase = int(176-110/(1+pow(2.73,((50-rightEncoder)*0.05))));
+        leftBase = int(180-110/(1+pow(2.73,((50-leftEncoder)*0.05))));
+        turnLeft();  
+    }
+    brake();
 }
