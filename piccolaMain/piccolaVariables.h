3bb4dbcb02fc35fcb66aaf95a2b01455fac6fa17
Amaya Dharmasiri <32506203+theamaya@users.noreply.github.com>
2019-09-02 19:38:36 +0530

    Update piccolaVariables.h

--- piccolaMain/piccolaVariables.h
+++ piccolaMain/piccolaVariables.h
@@ -19,25 +19,25 @@ int rightWall  = 1;
 int leftWall = 1;
 
 float leftP = 0.8;
-float leftD = 3.4;
-//float leftI=0.01;
+float leftD = 0;//3.4;
+float leftI=0.01;
 
 float leftError = 0;
 float leftLastError = 0;
-//float leftSumError=0;
+float leftSumError=0;
 
 float rightP = 0.8;
-float rightD = 3.4;
-//float rightI=0.01;
+float rightD =0;// 3.4;
+float rightI=0.01;
 
 float rightError = 0;
 float rightLastError = 0;
-//float rightSumError=0;
+float rightSumError=0;
 
 float wallP = 0.8;
-float wallI = 0;
-float wallD = 2.7;
-//float wallI=0.01;
+//float wallI = 0;
+float wallD = 0;// 2.7;
+float wallI=0.01;
 
 float wallError = 0;
 float wallLastError = 0;
@@ -52,6 +52,6 @@ unsigned long time;
 
 boolean left,right,front;
 int cell =0;
-boolean  L = true;
+/*boolean  L = true;
 boolean  R= true;
-boolean  F= false; 
+boolean  F= false; */
