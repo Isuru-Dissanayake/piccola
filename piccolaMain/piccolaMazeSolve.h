a74f8a0c14760e923cd75a8e1f953c617c452118
KavindaDulhan <kavindadulhan@gmail.com>
2019-09-04 16:09:42 +0530

    Update piccolaMazeSolve.h

--- piccolaMain/piccolaMazeSolve.h
+++ piccolaMain/piccolaMazeSolve.h
@@ -1,71 +1,67 @@
-void mazeSolve()  /// a right priority algorithm
-{
-    tofPid();
-    tofPid();
-    checkWallsCell();
-    if (cellWalls[2] == 0)
-    {
-        nextMove = 'R';
-    }
+void calculatePath(boolean runningNext,boolean eeprom){
+  floodFill2();
+  x=0;
+  y=0;
+  //char prevDir;
+  byte oldOrient= orient;
+  
+  cellCount=0;
+  //prevDir= dir;
+  
+    while(flood2[y][x]!=1){
 
-    else if (cellWalls[1] == 0)
-    {
-        nextMove = 'F';
-    }
-
-    else if (cellWalls[0] == 0)
-    {
-        nextMove = 'L';
-    }
-
-    else if (cellWalls[0] == 1)
-    {
-        nextMove = 'B';
-    }
+        toMove2();
+        if (runningNext){
+        pathQueue.enqueue(dir);
+        }
+        if(eeprom){
+        cellCount++;
+        EEPROM.write(cellCount,dir);}
+        
     
-    switch (nextMove)
-    {
-    case 'F' :      //move forward
-        cellForward();
-        break;
-    case 'R' :      //move right
-        rightTurn();
-        break;
+        if (dir=='L'){
+            orient = orientation(orient,'L');
+        }
 
-    case 'L' :      //move left
-        leftTurn();
-        break;
-    
-    case 'B' :      //move back
-        cellBack();
-        break;
-    }
-    
+        else if (dir=='R'){
+            orient = orientation(orient,'R');
+        }
+
+        else if (dir=='B'){
+            orient = orientation(orient,'L');
+            orient = orientation(orient,'L');
+        }
+
+        
+        xprev=x;
+        yprev=y;
+        updateCoordinates();
+        //prevDir= dir;
+        
+  }
+        x=0;
+        y=0;
+        orient=oldOrient;   
 }
 
-
 void traverse(byte xdes, byte ydes, boolean middleSquare, boolean shortPath){
-  
+  //int forwardCellCount=0;
 
   if (shortPath== false){
   appendDestination(xdes,ydes,middleSquare);
   floodFill3();
-
-  tofPid();
-  tofPid();
+  //tofPid();
+  //tofPid();
   checkWallsCell();
   updateWalls(x, y, orient, L, R, F);
-  
   while(flood[y][x]!=0){
     
     //cells[y][x]= sliit[y][x];
-    
+    checkWallsCell();
+    updateWalls(x, y, orient, L, R, F);
     appendDestination(xdes,ydes,middleSquare);
     floodFill3();
     dir= toMove(x,y,xprev,yprev,orient);
-
-    //Serial.println(dir);
-    
         if (dir=='L'){
             orient = orientation(orient,'L');
             leftTurn();
@@ -84,66 +80,56 @@ void traverse(byte xdes, byte ydes, boolean middleSquare, boolean shortPath){
         else{
           if(x==0 && y==0){
             cellStart();
-            brake();
-            delay(1000);
+            //brake();
+            //delay(1000);
           }
           else{
           cellForward();
           }
         }
-        
-        
         xprev=x;
         yprev=y;
         updateCoordinates();
-        
-
   }
   }
 
   else{
-      while(flood2[y][x]!=1){
 
-
-    tofPid();
-    tofPid();
-    checkWallsCell();
-    updateWalls(x, y, orient, L, R, F);
-    toMove2();
-
-    //Serial.println(dir);
+    calculatePath(true,false);
+    while (!pathQueue.isEmpty ()){
+    dir= pathQueue.dequeue();
     
         if (dir=='L'){
             orient = orientation(orient,'L');
             leftTurn();
+            xprev=x;
+            yprev=y;
+            updateCoordinates();
         }
 
         else if (dir=='R'){
             orient = orientation(orient,'R');
             rightTurn();
+            xprev=x;
+            yprev=y;
+            updateCoordinates();
         }
 
         else if (dir=='B'){
             orient = orientation(orient,'L');
             orient = orientation(orient,'L');
             cellBack();
+            xprev=x;
+            yprev=y;
+            updateCoordinates();
         }
         else{
-          if(x==0 && y==0){
-            cellStart();
-            brake();
-            delay(1000);
-          }
-          else{
           cellForward();
+          xprev = x;
+          yprev = y;
+          updateCoordinates();
           }
-        }
-        
-        
-        xprev=x;
-        yprev=y;
-        updateCoordinates();
-        
+          
+        }   
   }
   }
-}
