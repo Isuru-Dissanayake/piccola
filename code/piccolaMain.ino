53cce7c66a90001f08425aedbec390f9fae8badb
KavindaDulhan <kavindadulhan@gmail.com>
2019-09-04 16:08:58 +0530

    Update piccolaMain.ino

--- code/piccolaMain.ino
+++ code/piccolaMain.ino
@@ -1,6 +1,8 @@
 #include <VL6180X.h>
 #include <Wire.h>
+#include "EEPROM.h"
 #include "piccolaPins.h"
+#include "piccolaZlgoVariables.h"
 #include "piccolaVariables.h"
 #include "piccolaTof.h"
 #include "piccolaMotors.h"
@@ -8,10 +10,11 @@
 #include "piccolaWallPid.h"
 #include "piccolaSpeed.h"
 #include "piccolaMoves.h"
+#include "piccolaZlgo.h"
 #include "piccolaMazeSolve.h"
 
 void setup()
-{
+{   stbyHigh();
     tofSetup();
     motorDiver();
     motorInterrupt();
@@ -24,6 +27,43 @@ void setup()
 
 void loop()
 {
-    tofCell();
+    tofPid();
+    if (tof[2] <80)
+    {
+        start = 1;
+    }
+    if (start == 1)
+    {
+        delay(1000);
+        cellStart();
+        x=0;
+        y=1;
+        cells[0][0]= 11;
+        traverse(0,0,true,false);
+        center();
+        brake();
+        delay(5000);
+        traverse(13,0,false,false);
+        brake();
+        delay(5000);
+
+        traverse(0,0,false,false); 
+        floodFill2();
+        traverse(0,0,true,true);
+        brake();
+        delay(5000);
+        traverse(0,13,false,false);
+        brake();
+        delay(5000);
+
+        traverse(0,0,false,false); 
+        while(1){brake();} 
+    }
+}
+
+
+void loopllll()
+{
+    tofPid();
     printTof();
 }
