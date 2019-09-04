685e5ce453d6d14bf2288dd48b965d83a1258fbf
Amaya Dharmasiri <32506203+theamaya@users.noreply.github.com>
2019-09-04 13:53:50 +0530

    Update piccolaMazeSolve.h

--- arduinoAlgoCheck/piccolaMazeSolve.h
+++ arduinoAlgoCheck/piccolaMazeSolve.h
@@ -1,75 +1,31 @@
-/*void mazeSolve()  /// a right priority algorithm
-{
-    tofPid();
-    tofPid();
-    checkWallsCell();
-    if (cellWalls[2] == 0)
-    {
-        nextMove = 'R';
-    }
 
-    else if (cellWalls[1] == 0)
-    {
-        nextMove = 'F';
-    }
-
-    else if (cellWalls[0] == 0)
-    {
-        nextMove = 'L';
-    }
-
-    else if (cellWalls[0] == 1)
-    {
-        nextMove = 'B';
-    }
-    
-    switch (nextMove)
-    {
-    case 'F' :      //move forward
-        cellForward();
-        break;
-    case 'R' :      //move right
-        rightTurn();
-        break;
-
-    case 'L' :      //move left
-        leftTurn();
-        break;
-    
-    case 'B' :      //move back
-        cellBack();
-        break;
-    }
-    
-}
-*/
-
-
-void calculatePath(){
+void calculatePath(boolean runningNext,boolean eeprom){
   floodFill2();
   showFlood2();
   
   x=0;
   y=0;
-  char prevDir;
+  //char prevDir;
   byte oldOrient= orient;
-  int forwardCellCount =0;
   
-  //toMove2();
-  prevDir= dir;
+  cellCount=0;
+  //prevDir= dir;
   
     while(flood2[y][x]!=1){
 
         toMove2();
 
         //Serial.println(20);
-        Serial.print(x); Serial.print(' '); Serial.println(y);
-        Serial.println(dir);
-
+        //Serial.print(x); Serial.print(' '); Serial.println(y);
+        //Serial.println(dir);
         
+        if (runningNext){
         pathQueue.enqueue(dir);
+        }
+        if(eeprom){
+        cellCount++;
+        EEPROM.write(cellCount,dir);}
         
-
     
         if (dir=='L'){
             orient = orientation(orient,'L');
@@ -88,7 +44,7 @@ void calculatePath(){
         xprev=x;
         yprev=y;
         updateCoordinates();
-        prevDir= dir;
+        //prevDir= dir;
         
   }
   
@@ -168,7 +124,7 @@ void traverse(byte xdes, byte ydes, boolean middleSquare, boolean shortPath){
 
   else{
 
-    calculatePath();
+    calculatePath(true,false);
     showFlood2();
     
     while (!pathQueue.isEmpty ()){
