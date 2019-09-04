06bb2c847552e877611b47ad6c70ddc9c807ebe6
Amaya Dharmasiri <32506203+theamaya@users.noreply.github.com>
2019-09-05 01:36:10 +0530

    Update piccolaMain.ino

--- piccolaMain/piccolaMain.ino
+++ piccolaMain/piccolaMain.ino
@@ -1,6 +1,6 @@
 #include <VL6180X.h>
 #include <Wire.h>
-#include "EEPROM.h"
+#include <EEPROM.h>
 #include "piccolaPins.h"
 #include "piccolaZlgoVariables.h"
 #include "piccolaVariables.h"
@@ -18,11 +18,13 @@ void setup()
     tofSetup();
     motorDiver();
     motorInterrupt();
+    pinMode(buzzer, OUTPUT);
     attachInterrupt(digitalPinToInterrupt(PB12), countLeftOut1, RISING);
     attachInterrupt(digitalPinToInterrupt(PB13), countLeftOut1, RISING);
     attachInterrupt(digitalPinToInterrupt(PB14), countRightOut1, RISING);
     attachInterrupt(digitalPinToInterrupt(PB15), countRightOut1, RISING);
     Serial2.begin(9600);
+    delay(1000);
 }
 
 void loop()
@@ -30,20 +32,63 @@ void loop()
     tofPid();
     if (tof[2] <80)
     {
+        buzz();
+        delay(500);
         start = 1;
     }
     if (start == 1)
     {
         delay(1000);
-        traverse(0,0,true,false, false);
+        /*traverse(0,0,true,false,false);
+        checkWallsCell();
+        updateWalls(x, y, orient, L, R, F);
+        cellBrake();
+        
         center();
+        
         calculatePath(false,true);
-        brake();
-        delay(5000);
+        delay(3000);
+
         traverse(0,0,false,false,false);
-        floodFill2();
+        cellBrake();
+        fixOrientation();
+        
+        delay(1000);
+       
+        floodFill2();*/
         traverse(0,0,true,true,true);
-        brake();
         while(1){brake();} 
     }
+}
+void loopu(){
+  for (int i=0; i<14;i++){
+    for (int j=0; j<14;j++){
+      cells[j][i]=sliit[j][i];
+    }
+  }
+
+  Serial2.println(1000);
+  calculatePath(false,true);
+  /*for (int j=0; j<14;j++){
+      EEPROM.write(j,j);
+    }
+
+  for (int j=0; j<14;j++){
+      Serial2.println(EEPROM.read(j));
+    }*/
+
+  cellCount= EEPROM.read(0);
+  char shortestPath[cellCount];
+
+  for( int i=0; i<cellCount;i++){
+
+        shortestPath[i]= char(EEPROM.read(i+1));
+  }
+
+for (int i=0; i<cellCount; i++){
+  dir = shortestPath[i];
+  Serial2.println(dir);
+  }
+  while(1){}
+  
 }
