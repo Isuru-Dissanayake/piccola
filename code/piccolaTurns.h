f131db6e0abe2c4681d84f1620105e6c3d81f686
KavindaDulhan <kavindadulhan@gmail.com>
2019-09-08 13:09:47 +0530

    Update piccolaTurns.h

--- code/piccolaTurns.h
+++ code/piccolaTurns.h
@@ -111,8 +111,8 @@ void leftAboutTurn()
     }
     rightBase=176;
     leftBase=180;
-    encoderRightCount= encoderRightCount + 250;
-    encoderLeftCount= encoderLeftCount + 250;
+    encoderRightCount= encoderRightCount + 240;
+    encoderLeftCount= encoderLeftCount + 240;
     while(rightEncoder <= encoderRightCount)
     {
         turnLeft();
@@ -135,3 +135,47 @@ void leftAboutTurn()
     leftEncoder = 0;
     rightEncoder = 0;
 }
+
+void rightHalfTurn()
+{
+    encoderLeftCount = 0;
+    encoderRightCount = 0;
+    leftEncoder = 0;
+    rightEncoder = 0;
+    encoderRightCount= encoderRightCount + 100;
+    encoderLeftCount= encoderLeftCount + 100;
+    rightBase = 70;
+    leftBase = 70;
+    while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
+    {
+        dif = leftEncoder - encoderLeftCount + 100;
+        rightBase = int(70+110/(1+pow(2.73,((50-dif)*0.05))));
+        leftBase = int(70+110/(1+pow(2.73,((50-dif)*0.05))));
+        turnRight();
+    }
+    rightBase=176;
+    leftBase=180;
+    encoderRightCount= encoderRightCount + 1;
+    encoderLeftCount= encoderLeftCount + 1;
+    while(rightEncoder <= encoderRightCount)
+    {
+        turnRight();
+    }
+    encoderRightCount= encoderRightCount + 100;
+    encoderLeftCount= encoderLeftCount + 100;
+    while (rightEncoder <= encoderRightCount || leftEncoder <= encoderLeftCount)
+    {
+        dif = leftEncoder - encoderLeftCount + 100;
+        rightBase = int(176-110/(1+pow(2.73,((50-dif)*0.05))));
+        leftBase = int(180-110/(1+pow(2.73,((50-dif)*0.05))));
+        turnRight();  
+    }
+    brake();
+    leftBase = 180;
+    rightBase = 176;
+    encoderLeftCount = 0;
+    encoderRightCount = 0;
+    leftEncoder = 0;
+    rightEncoder = 0;
+}
+
